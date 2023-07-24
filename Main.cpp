94741abe457333fa01f61719f087bf33358104a8
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-07-24 17:45:48 -0500

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 
-void Function()
+struct Entity
 {
+	int x, y;
+	
+	static void Print()
+	{
+		std::cout << x << ", " << y << std::endl;
+	}
+};
 
+static void Print()
+{
+	std::cout << x << ", " << y << std::endl;
 }
 
 int main()
 {
+	Entity e;
+	e.x = 2;
+	e.y = 3;
+
+	Entity e1;
+	e1.x = 5;
+	e1.y = 8;
+
+	Entity::Print();
+	Entity::Print();
 	std::cin.get();
 }
