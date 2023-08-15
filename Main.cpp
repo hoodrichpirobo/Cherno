6e14e66c177c4cbf97c54092c3bd4256199f38b4
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-08-15 15:10:26 -0500

    Interfaces in C++ (Pure Virtual Functions)

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
 
-class Entity
+class Printable
+{
+public:
+	virtual std::string GetClassName() = 0;
+};
+
+class Entity : public Printable
 {
 public:
 	virtual std::string GetName() { return "Entity"; }
+	std::string GetClassName() override { return "Entity"; }
 };
 
 class Player : public Entity
@@ -15,7 +22,9 @@ public:
 	Player(const std::string& name)
 		: m_Name(name) {}
 
-		std::string GetName() override { return m_Name; }
+	std::string GetName() override { return m_Name; }
+	std::string GetClassName() override { return "Player"; }
+
 };
 
 void PrintName(Entity* entity)
@@ -23,13 +32,29 @@ void PrintName(Entity* entity)
 	std::cout << entity->GetName() << std::endl;
 }
 
+class A : public Printable
+{
+public:
+	std::string GetClassName() override { return "A"; }
+};
+
+void Print(Printable* obj)
+{
+	std::cout << obj->GetClassName() << std::endl;
+}
+
+
 int main()
 {
 	Entity* e = new Entity();
-	PrintName(e);
+	//PrintName(e);
 
 	Player* p = new Player("Cherno");
-	PrintName(p);
+	//PrintName(p);
+
+	Print(e);
+	Print(p);
+	Print(new A());
 
 	std::cin.get();
 }
